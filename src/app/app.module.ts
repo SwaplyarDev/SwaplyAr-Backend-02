96846b67284f118e9b9dea81e92be6be43d956d5
Alan Varde <alan.varde@gmail.com>
2025-04-29 10:15:00 -0300

    feat: add user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@config/config.module';
 import typeormConfig from '@config/typeorm.config';
+import { UserModule } from '@user/user.module';
 
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forRootAsync(typeormConfig.asProvider()),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [],

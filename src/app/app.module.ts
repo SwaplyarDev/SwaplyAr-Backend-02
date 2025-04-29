d1bfdcc5ba5753278059acea2d0ee32319f7ed67
Alan Varde <alan.varde@gmail.com>
2025-04-29 10:52:49 -0300

    feat: add transactions module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@config/config.module';
 import typeormConfig from '@config/typeorm.config';
 import { UsersModule } from '@users/users.module';
+import { TransactionsModule } from '@transactions/transactions.module';
 
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forRootAsync(typeormConfig.asProvider()),
     UsersModule,
-
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [],

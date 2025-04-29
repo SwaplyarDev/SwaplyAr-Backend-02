79844ae58dca3a38490c1533051de76d35bd949a
Alan Varde <alan.varde@gmail.com>
2025-04-29 10:14:15 -0300

    fix: avoid having to configure dotenv in typeorm config file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,20 +1,18 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as process from 'node:process';
-import { config as dotenvConfig } from 'dotenv';
 
-dotenvConfig();
+export default registerAs('typeorm', (): TypeOrmModuleOptions => {
+  const isProduction = process.env.NODE_ENV === 'production';
 
-const typeormConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD,
-  synchronize: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  dropSchema: true, // CUIDADO, NO UTILIZAR CON DB DE PRODUCCIÓN
-};
-
-export default registerAs('typeorm', () => typeormConfig);
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD,
+    synchronize: !isProduction,
+    autoLoadEntities: true,
+    dropSchema: !isProduction,
+  };
+});

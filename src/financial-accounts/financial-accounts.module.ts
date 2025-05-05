a3a9f1134f00959792f6cf65041332cd495f640f
Agusmalugani <agusmalugani97@gmail.com>
2025-05-05 12:23:03 -0300

    .

--- src/financial-accounts/financial-accounts.module.ts
+++ src/financial-accounts/financial-accounts.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SenderFinancialAccountsModule } from './sender-financial-accounts/sender-financial-accounts.module';
 import { ReceiverFinancialAccountsModule } from './receiver-financial-accounts/receiver-financial-accounts.module'; 
 import { ProofOfPaymentsModule } from './proof-of-payments/proof-of-payments.module';
-import { PaymentMethodModule } from './payment-method/payment-method.module';
+import { PaymentMethodModule } from './payment-method/payment-method.module'; 
 
 @Module({
   controllers: [],

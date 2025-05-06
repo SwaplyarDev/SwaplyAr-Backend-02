778ec664d4fa9a89011e502b4192f0b45f74ecf7
Alan Varde <alan.varde@gmail.com>
2025-05-06 12:29:02 -0300

    fix: remove unnecessary typeorm repository imports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,35 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '@users/entities/user.entity';
-import { UserProfile } from '@users/entities/user-profile.entity';
-import { UserAlternativeEmail } from '@users/entities/user-alternative-email.entity';
-import { UserLocation } from '@users/entities/user-location.entity';
-import { UserSocials } from '@users/entities/user-socials.entity';
-import { UserRewardsLedger } from '@users/entities/user-rewards-ledger.entity';
-import { UserCategory } from '@users/entities/user-category.entity';
-import { UserContact } from '@users/entities/user-contact.entity';
-import { UserQuestion } from '@users/entities/user-question.entity';
-import { UserDiscount } from '@users/entities/user-discount.entity';
-import { DiscountCode } from '@users/entities/discount-code.entity';
-import { UserBan } from '@users/entities/user-ban.entity';
 
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      DiscountCode,
-      User,
-      UserAlternativeEmail,
-      UserBan,
-      UserCategory,
-      UserContact,
-      UserDiscount,
-      UserLocation,
-      UserProfile,
-      UserQuestion,
-      UserRewardsLedger,
-      UserSocials,
-    ]),
-  ],
-  exports: [TypeOrmModule],
-})
+@Module({})
 export class UsersModule {}

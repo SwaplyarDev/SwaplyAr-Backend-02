1b062cb72efed3ab9588b5c64d204e030c06f68c
Agusmalugani <agusmalugani97@gmail.com>
2025-05-14 12:47:40 -0300

    fix:

--- src/modules/transactions/amounts/dto/create-amount.dto.ts
+++ src/modules/transactions/amounts/dto/create-amount.dto.ts
@@ -2,5 +2,5 @@ export class CreateAmountDto {
     amountSent: number; // cantidad enviada
     currencySent: string; // moneda enviada
     amountReceived: number; // cantidad recibida
-    currencyReceived: string; // moneda recibida
+    currencyReceived: string; // moneda recibida 
 }

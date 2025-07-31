5ff0838297d70f871d65df7fc123ef069da05b16
dylan <devdylancrowder@outlook.com>
2025-07-31 12:55:21 -0300

    fix:role en /users/edit

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -94,7 +94,7 @@ export class UsersController {
   // EDITAR INFORMACION DE USUARIO
   @Patch('/edit/:userId')
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @Roles('user')
   @ApiBearerAuth()
   @UseInterceptors(ClassSerializerInterceptor)
   @ApiOperation({ summary: 'Editar información del usuario' })

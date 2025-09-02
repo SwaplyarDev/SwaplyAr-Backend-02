00a6e18099a97e7f9149cb47d38e64ecc1a19ebe
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-02 11:41:24 -0300

    fix: correccion en validacion de transactionId de UUID a Varchar

--- src/modules/discounts/dto/update-user-discount.dto.ts
+++ src/modules/discounts/dto/update-user-discount.dto.ts
@@ -1,8 +1,10 @@
-import { IsUUID } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDiscountDto {
   @ApiProperty({ description: 'ID de la transacción que utiliza el descuento' })
-  @IsUUID()
+  @IsString()
+  @MinLength(10, { message: 'El ID de la transacción debe tener al menos 10 caracteres.' })
+  @MaxLength(10, { message: 'El ID de la transacción no puede exceder los 10 caracteres.' })
   transactionId: string;
 }

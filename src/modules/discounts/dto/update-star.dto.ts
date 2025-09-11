fa60b1d21b0d1dba677170eebe4b4ebf3c13f6d7
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2025-09-11 11:37:59 -0500

    fix(discounts): resolve 500 error in update-star by correcting invalid request body and enhancing UpdateStarDto (validation + Swagger examples)

--- src/modules/discounts/dto/update-star.dto.ts
+++ src/modules/discounts/dto/update-star.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, Min } from 'class-validator';
+/*import { IsInt, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateStarDto {
@@ -8,5 +8,27 @@ export class UpdateStarDto {
   @IsInt()
   @Min(1)
   quantity: number;
+  transactionId: string;
+}*/
+
+import { IsInt, Min, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+
+export class UpdateStarDto {
+  @ApiProperty({
+    description: 'Monto de la transacción a añadir a las recompensas',
+    example: 100, 
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  quantity: number;
+
+  @ApiProperty({
+    description: 'Id de la transacción asociada',
+    example: 'vzGua5nfRo',   
+  })
+  @IsString()
   transactionId: string;
 }

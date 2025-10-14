aa99e5e094205ac97c8a326235e4ef4472fe8ea9
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2025-10-14 12:05:13 -0500

    chore(temp): commit provisional antes de traer snippet de develop.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { DiscountModule } from 'src/modules/discounts/discounts.module';
 import { QualificationModule } from 'src/modules/qualifications/qualifications.module';
 import { ConversionsModule } from 'src/modules/conversions/conversions.module';
 import { CommissionsModule } from 'src/modules/conversions/commissions/commissions.module';
+import { DynamicCommissionsModule } from 'src/modules/dynamic-commissions/dynamicCommissions.module';
 
 @Module({
   imports: [
@@ -51,6 +52,7 @@ import { CommissionsModule } from 'src/modules/conversions/commissions/commissio
     QualificationModule,
     ConversionsModule,
     CommissionsModule,
+    DynamicCommissionsModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'tu_clave_secreta',
       signOptions: { expiresIn: '1d' },

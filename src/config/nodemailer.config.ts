b49ee2af27dc8034009eaba2d14a8f18a74f9192
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2025-10-14 11:24:39 -0300

    refactor(nodemailer): actualizar configuración para usar SMTP directamente y agregar opciones de conexión

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -3,10 +3,14 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export default registerAs('nodemailer', (): SMTPTransport.Options => {
   return {
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    debug: true,
   };
 });
